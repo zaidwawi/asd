c0e42e3b07fde90a8c2c5aeab061b41fd3272e7c
zaid wawi <76659815+zaidwawi@users.noreply.github.com>
2022-02-04 17:17:53 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 export FLASK_APP=app.py
-export DATABASE_URL='postgresql://postgres:zaidwawi056@localhost:5432/yarrb'
+export DATABASE_URL='postgresql://wltncrvmvdmubp:7ed5549d441e5db3ad07def0c1dcd47dedc3fdfe278e1769e91dee79ca0f0b6e@ec2-54-157-15-228.compute-1.amazonaws.com:5432/d5sn5pnimeiv1c'
